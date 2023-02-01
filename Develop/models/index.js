153801c38e7c54c25020f2039fc30980c3c6dfac
bmigo <berniecmigo@gmail.com>
2023-02-01 16:22:03 -0500

    index.js

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -11,9 +11,13 @@ Product.belongsTo(Category, {
 });
 
 // Categories have many Products
-
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, )
 
 // Tags belongToMany Products (through ProductTag)
 
